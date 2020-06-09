c250f3caf1f26bfcf0cf10ff70a4b5038b39ad0d
Eric Chen <ericchen@new-host-4.home>
2020-06-08 20:34:21 -0400

    slight edits and IP hosting

--- pythoncamp_project/settings.py
+++ pythoncamp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['0.0.0.0' , '127.0.0.1', '*']
+ALLOWED_HOSTS = ['0.0.0.0' , '127.0.0.1', '18.222.110.225']
 
 
 # Application definition
