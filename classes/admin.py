22031d0c0701468aad06384d2a37557aee3fc065
Eric Chen <ericchen@localhost.localdomain>
2021-02-03 03:53:22 -0500

    some inline admin changes

--- classes/admin.py
+++ classes/admin.py
@@ -6,26 +6,29 @@ from .models import Class, Payment, Introduction
 
 
 
+class IntroductionInline(admin.StackedInline):
+
+    model = Introduction
+
 class ClassAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'show_users', 'archived')
     ordering = ('archived', 'name')
 
-    # def get_tournaments(self, obj):
-    #     return "\n".join([p.users for p in obj.users.all()])
+    inlines = [IntroductionInline, ]
 
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('user', 'theclass', 'id', 'paid',)
 
-class IntroductionAdmin(admin.ModelAdmin):
+# class IntroductionAdmin(admin.ModelAdmin):
 
-    list_display = (
-        'title', 'course',
-    )
+#     list_display = (
+#         'title', 'course',
+#     )
 
 admin.site.register(Class, ClassAdmin)
 
 
 admin.site.register(Payment, PaymentAdmin)
 
-admin.site.register(Introduction, IntroductionAdmin)
+# admin.site.register(Introduction, IntroductionAdmin)
--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import UserProfile
 
+from classes.models import Payment
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -13,12 +14,15 @@ from .signupform import CustomSignupForm
 CustomUser = get_user_model()
 
 
+class PaymentInline(admin.TabularInline):
+    model=Payment
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'first_name', 'last_name','school', 'parent_email', 'grade']
-    
+    inlines = [  PaymentInline, ]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
